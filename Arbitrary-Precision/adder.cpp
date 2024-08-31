f47488783fc9c55c759bfa6670b0ae17c115de25
Han-Xiangming <2728513634@qq.com>
2024-08-31 20:16:54 +0800

    add:Arbitrary-Precision Arithmetic

---
+++ Arbitrary-Precision/adder.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+const int LEN = 1004;
+
+int a[LEN], b[LEN], c[LEN];
+
+void clear(int a[]) {
+  for (int i = 0; i < LEN; ++i) a[i] = 0;
+}
+
+void read(int a[]) {
+  string s;
+  cin >> s;
+
+  clear(a);
+
+  int len = s.length();
+  for (int i = 0; i < len; ++i) a[len - i - 1] = s[i] - '0';
+}
+
+void print(int a[]) {
+  int i;
+  for (i = LEN - 1; i >= 1; --i)
+    if (a[i] != 0) break;
+  for (; i >= 0; --i) cout << a[i];
+  cout << endl;
+}
+
+void add(int a[], int b[], int c[]) {
+  clear(c);
+
+  for (int i = 0; i < LEN - 1; ++i) {
+    c[i] += a[i] + b[i];
+    if (c[i] >= 10) {
+      c[i + 1] += 1;
+      c[i] -= 10;
+    }
+  }
+}
+
+int main() {
+  read(a);
+  read(b);
+
+  add(a, b, c);
+  print(c);
+
+  return 0;
+}
--- demo.cpp
+++ demo.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
+#include <unordered_map>
+#define ll long long
 using namespace std;
 int main(){
-    int n1,n2,a[105],b[105],dp[105][105];
-    cin>>n1;for (int i=1;i<=n1;i++) cin>>a[i];
-    cin>>n2;for (int i=1;i<=n2;i++) cin>>b[i];
-    int ans=0;
-    for (int i=1;i<=n1;i++){
-        for (int j=1;j<=n2;j++){
-            dp[i][j]=(a[i]==b[j])?dp[i-1][j-1]+1:max(dp[i-1][j],dp[i][j-1]);
-            ans=max(ans,dp[i][j]);
-        }
-        
-    }
-    cout<<ans<<endl;
+    unordered_map<ll,string> mp;
+    mp[1]="one";
+    mp[2]="two";
+    mp[3]="three";
+    mp[4]="four";
+    mp[5]="five";
+    mp[6]="six";
+    mp[7]="seven";
+    mp[8]="eight";
+    mp[9]="nine";
+    mp[10]="ten";
+    mp.erase(1);
+    ll n;cin>>n;
+    cout << mp[n] << endl;
 }

c69ca75eb1dd9687bc4c8a4494d2b9c6325c7c9b
Han-Xiangming <2728513634@qq.com>
2024-08-31 17:27:34 +0800

    Change some files

---
+++ demo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int n,a[105],dp[105];
+    cin>>n;
+    for (int i=1;i<=n;i++) cin>>a[i];
+    ll ans=0;
+    for (int i=1;i<=n;i++){
+        dp[i]=1;
+        for (int j=1;j<i;j++){
+            if (a[j]<a[i]) dp[i]=max(dp[i],dp[j]+1);
+        }
+        ans=max(ans,dp[i]);
+    }
+    cout<<ans<<endl;
+}

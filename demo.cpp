8d8f3008ac79f4d0ae9cd328fd061fa7a7f3ae9b
Han-Xiangming <2728513634@qq.com>
2024-09-08 13:07:01 +0800

    Change some files

--- demo.cpp
+++ demo.cpp
@@ -1,21 +1,3 @@
 #include <iostream>
-#include <string>
 #include <unordered_map>
-#define ll long long
 using namespace std;
-int main(){
-    unordered_map<ll,string> mp;
-    mp[1]="one";
-    mp[2]="two";
-    mp[3]="three";
-    mp[4]="four";
-    mp[5]="five";
-    mp[6]="six";
-    mp[7]="seven";
-    mp[8]="eight";
-    mp[9]="nine";
-    mp[10]="ten";
-    mp.erase(1);
-    ll n;cin>>n;
-    cout << mp[n] << endl;
-}

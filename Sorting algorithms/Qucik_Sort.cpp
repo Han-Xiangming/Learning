834a7e9f650d3c32bf81fb2b874fe9981af35451
Han-Xiangming <2728513634@qq.com>
2024-09-08 15:39:28 +0800

    Change some files

--- Sorting algorithms/Qucik_Sort.cpp
+++ Sorting algorithms/Qucik_Sort.cpp
@@ -2,38 +2,25 @@
 #include <vector> 
 using namespace std;
 
-void swap(vector<int>::iterator xp, vector<int>::iterator yp) {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
 
-int partition(vector<int>& vec, int low, int high) {
-    int pivot = vec[high];
-    int i = (low - 1);
-
-    for (int j = low; j <= high - 1; j++) {
-        if (vec[j] < pivot) {
-            i++;
-            swap(vec.begin() + i, vec.begin() + j);
-        }
+void quickSort(int l,int r,vector<int>& arr){
+    if (l>=r) return;
+    int pivot = arr[l];
+    int i = l, j = r;
+    while (i < j) {
+        while (arr[i] <= pivot && i < r) i++;
+        while (arr[j] > pivot && j > l) j--;
+        if (i < j) swap(arr[i], arr[j]);
     }
-    swap(vec.begin() + i + 1, vec.begin() + high);
-    return (i + 1);
-}
-
-void quickSort(vector<int>& vec, int low, int high) {
-    if (low < high) {
-        int pi = partition(vec, low, high);
-        quickSort(vec, low, pi - 1);
-        quickSort(vec, pi + 1, high);
-    }
+    swap(arr[l], arr[j]);
+    quickSort(l, j - 1, arr);
+    quickSort(j + 1, r, arr);
 }
 
 int main() {
     vector<int> vec = {10, 7, 8, 9, 1, 5};
     int n = vec.size();
-    quickSort(vec, 0, n - 1);
+    quickSort(0, n - 1, vec);
     cout << "Sorted array: \n";
     for (const auto& i : vec) cout << i << " ";
     return 0;

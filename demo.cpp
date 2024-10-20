a7a86aec39656f1909196224f77ef5d4faa2b670
Han-Xiangming <2728513634@qq.com>
2024-10-20 14:30:01 +0800

    Change some files

--- demo.cpp
+++ demo.cpp
@@ -1,11 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+int w[105], val[105];
+int dp[105][1005];
 int main(){
-    ll n,L,R,ans=0;
-    cin>>n>>L>>R;
-    for (int i = L;i<=R;i++){
-        ans = max(ans,i%n);
-    }
-    cout << ans << endl;
+    int t, m, res = -1;
+    cin >> t >> m;
+    for (int i = 1; i <= m; i++) cin >> w[i] >> val[i];
+    for (int i = 1; i <= m; i++)
+        for (int j = t; j >= 0; j--){
+            if (j >= w[i]) dp[i][j] = max(dp[i - 1][j - w[i]] + val[i], dp[i - 1][j]);
+            else dp[i][j] = dp[i - 1][j];
+        }
+    cout << dp[m][t];
+    return 0;
 }

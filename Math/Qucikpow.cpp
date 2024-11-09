ee043dec42e7fc1b0e4dd9353a4302711d288ec1
Han-Xiangming <2728513634@qq.com>
2024-11-09 18:42:50 +0800

    Change some files

---
+++ Math/Qucikpow.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define file(x) freopen(x".in","r",stdin),freopen(x".out","w",stdout);
+
+ll quickpow(ll a, ll b, ll mod) {
+    ll ret = 1;
+    while(b){
+        if(b%2==1) ret = (ret*a)%mod;
+        a = (a*a)%mod;
+        b /= 2;
+    }return ret;
+}
+
+int main(){
+    ll a, b, mod;
+    cin >> a >> b >> mod;
+    cout << quickpow(a, b, mod) << endl;
+    return 0;
+}

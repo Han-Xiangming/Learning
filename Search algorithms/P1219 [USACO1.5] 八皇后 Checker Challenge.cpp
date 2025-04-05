327f144cd521a07e2dd6c7d158f06d3d56f4ce3c
Han-Xiangming <2728513634@qq.com>
2025-04-05 20:41:47 +0800

    Change some files

--- Search algorithms/P1219 [USACO1.5] 八皇后 Checker Challenge.cpp
+++ Search algorithms/P1219 [USACO1.5] 八皇后 Checker Challenge.cpp
@@ -10,8 +10,7 @@ int read(){
 }
 void dfs(int line){
     if (line > n){
-        sum++;
-        if (sum>3) return;
+        if (++sum>3) return;
         else {
             for (int i=1;i<=n;i++) cout << ans[i] << " ";
             cout << '\n';
@@ -21,13 +20,9 @@ void dfs(int line){
     for(int i=1;i<=n;i++){
         if(!check[0][i] && !check[1][line+i] && !check[2][line-i+n]){
             ans[line]=i;
-            check[0][i]=1;
-            check[1][line+i]=1;
-            check[2][line-i+n]=1;
+            check[0][i] = check[1][line + i] = check[2][line - i + n] = 1;
             dfs(line+1);
-            check[0][i]=0;
-            check[1][line+i]=0;
-            check[2][line-i+n]=0;
+            check[0][i] = check[1][line + i] = check[2][line - i + n] = 0;
         }
     }
 }
